11f4df882188acdeb9372bd9a30beb0ad66da1c9
gr1ntch <odewumimighty@gmail.com>
2024-12-15 01:14:02 +0100

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cinzel_Decorative, Geist, Geist_Mono, UnifrakturMaguntia } from "next/font/google";
+import { Cinzel } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,18 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const cinzel = Cinzel_Decorative({
+  variable: "--font-cinzel",
+  subsets: ["latin"],
+  weight: "400",
+})
+
+const unifrak = UnifrakturMaguntia({
+  variable: "--font-unifrak",
+  subsets: ["latin"],
+  weight: "400"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +38,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${unifrak.variable} antialiased`}
       >
         {children}
       </body>
