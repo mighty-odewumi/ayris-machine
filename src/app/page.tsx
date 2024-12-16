700db3f5bb22ff02fefc42c54b1b85573db963f8
gr1ntch <odewumimighty@gmail.com>
2024-12-16 20:15:16 +0100

    Added Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 // import Image from "next/image";
-import main from "../../public/assets/welcome-home.png";
+// import main from "../../public/assets/welcome-home.png";
+
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={`bg-[url(${main})] bg-cover w-f ull`}>
-      
+    <div className={` bg-cover w-full h-full`}>
+      <Image 
+        src="/assets/welcome-home.png"
+        alt="Home page"
+        className="w-full h-full"
+        width={1900}
+        height={3000}
+      />
     </div>
   );
 }
