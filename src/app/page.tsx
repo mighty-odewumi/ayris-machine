f25d97a5479b1c36a21427e249b131c829119918
gr1ntch <odewumimighty@gmail.com>
2024-12-26 22:19:09 +0100

    Added dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 // import ImgMap from "@/components/responsiveMap";
+import dynamic from "next/dynamic";
 
 export default function Home() {
 
+  const MyMap = dynamic(
+    () => import('@/components/responsiveMap'),
+    { ssr: false }
+  )
+
   return (
     <main className={`container bg-[url("../../public/assets/background.png")]`}>
-      {/* <ImgMap /> */}
+      <MyMap />
     </main>
   );
 }
