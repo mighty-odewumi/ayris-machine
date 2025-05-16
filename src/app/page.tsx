829c920c954c15a899d4322ff2b1a6f44b0ec1bd
gr1ntch <odewumimighty@gmail.com>
2025-05-16 01:37:46 +0100

    Create image rendering on scroll but comment out for future use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import ScrollBottomLinks from "@/components/ScrollBottomLinks";
 import { homePageText } from "@/constants/homePageText";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <>
       <div id="home-intro">
+        
         <article className="home-article brown-article">
+          {/* <Image 
+            src="/assets/background.png" 
+            sizes="100vw"
+            alt="Background" 
+            width="200"
+            height="200"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }} 
+          /> */}
 
           <p className="mt-[-6rem] whitespace-pre-line">
             {homePageText}
